0f60958d516c1fce67f6c05f07634ca05b768dd2
cj <cjlazell@googlemail.com>
2010-12-31 13:31:55 -0600

    seperating the modules

--- application/config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASE') or exit('Access Denied!');  
-
-// put your application constants here.
-                                                          
-
-/* End of file constants.php */
-/* Location: ./application/config/constants.php */
--- modules/default/scripts/doly.php
+++
@@ -1,6 +0,0 @@
-
-<script type="text/javascript" language="Javascript">
-
-// Your inline javascript codes here.
-
-</script> 
